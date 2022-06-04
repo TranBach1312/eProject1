96943c84ac78f9a554cb7f23e8c9056970d48415
TranBach1312 <bach.tx.1978@aptechlearn.edu.vn>
2022-06-04 19:51:15 +0700

    create eProject1

---
+++ Application/layout/footer.php
@@ -0,0 +1,24 @@
+<?php
+    echo '    <footer>
+        <div class="f-top">
+            <div class="logo">
+                <a href="">
+                    <img src="" alt="">
+                </a>
+            </div>
+            <div class="about-us">
+                <h3>About US</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque vitae at impedit, autem rerum architecto eaque quae ex hic porro modi dolor iure expedita consequatur cupiditate repellat maiores cumque qui?</p>
+            </div>
+            <div>
+                <div class="social-icon"></div>
+                <div class="social-icon"></div>
+                <div class="social-icon"></div>
+                <div class="social-icon"></div>
+                <div class="social-icon"></div>
+                <div class="social-icon"></div>
+            </div>
+        </div>
+        <div class="f-bottom"></div>
+    </footer>';
+?>
